5635f3bdd24b4146d4f3406554251c64d587d28b
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-12 01:08:42 +0600

    set modal

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const BookingModal = ({ treatment }) => {
+    const { name, slots } = treatment;
     return (
         <div>
             <input type="checkbox" id="booking-modal" class="modal-toggle" />
-            <div class="modal modal-bottom sm:modal-middle">
-                <div class="modal-box">
-                    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            <div class="modal modal-bottom sm:modal-middle text-white ">
+                <div class="modal-box bg-white text-black">
+                    <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 class="font-bold text-lg ">Booking for : <span className='text-secondary'>{name}</span></h3>
+                    <p class="py-4">You've been selected for a </p>
                     <div class="modal-action">
                         <label for="booking-modal" class="btn">Yay!</label>
                     </div>
