24980067896ea080b78dc6f2b9a5c0a2db112cec
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-13 18:02:56 +0600

    firebase add and google login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div>
-            <h1>Log in</h1>
+        <div className='flex h-screen justify-center items-center'>
+            <div class="card w-96 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl">Login</h2>
+
+                    <div class="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline font-black"
+                    >Continue With Google</button>
+                </div>
+            </div>
         </div>
     );
 };
