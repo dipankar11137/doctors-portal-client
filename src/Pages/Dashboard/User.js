77eb4cce76862f4ac35b8fc8eaac274ac9ab5838
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-18 01:47:25 +0600

    add user email

--- src/Pages/Dashboard/User.js
+++ src/Pages/Dashboard/User.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
+
+
 
 const User = () => {
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
 
+    const { data: users, isLoading } = useQuery('user', () => fetch('http://localhost:5000/user').then(res => res.json()));
     if (isLoading) {
         return <Loading />
     }
+    // console.log(users[0].email)
 
     return (
         <div>
             <h2 className='text-2xl p-2'>All User {users.length}</h2>
-        </div>
+
+            <table className="table w-full text-white m-2">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Time</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(user => <UserRow key={user._id} user={user}></UserRow>)
+
+                    }
+                </tbody>
+            </table>
+        </div >
     );
 };
 
---
+++ src/Pages/Dashboard/UserRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserRow = ({ user }) => {
+    return (<tr>
+        <th>1</th>
+        <td>{user.email}</td>
+        <td>{user.email}</td>
+        <td>{user.email}</td>
+
+    </tr>
+
+
+    );
+};
+
+export default UserRow;
